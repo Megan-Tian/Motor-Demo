82ab36cf741af27dca2d0e7d206cffc94189baa1
Megan-Tian <52009990+Megan-Tian@users.noreply.github.com>
2022-10-26 22:08:22 -0700

    Updated ShooterCommand to be variable based on position of joystick

--- src/main/java/frc/robot/commands/ShooterCommand.java
+++ src/main/java/frc/robot/commands/ShooterCommand.java
@@ -5,6 +5,7 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.RobotContainer;
 import frc.robot.subsystems.ShooterSubsystem;
 
 public class ShooterCommand extends CommandBase {
@@ -24,7 +25,8 @@ public class ShooterCommand extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    m_shooterSubsystem.spinShooter(0.5);
+    System.out.println(RobotContainer.r_joystick.getY());
+    m_shooterSubsystem.spinShooter(RobotContainer.r_joystick.getY());
   }
 
   // Called once the command ends or is interrupted.
